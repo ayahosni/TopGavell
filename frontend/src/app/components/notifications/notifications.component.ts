7c504d31a7be459c4564a5ee0aa2ad62429ae90e
aya <ayahosny4817@gmail.com>
2024-10-12 18:31:49 +0300

    user profile

--- frontend/src/app/components/notifications/notifications.component.ts
+++ frontend/src/app/components/notifications/notifications.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { NotificationService } from '../../services/notification.service'; 
-
-import { RouterModule, Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { NotificationService } from '../../services/notification.service';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+interface Notification {
+  id: string;
+  data: {
+    auction: string;  // اسم المزاد
+    user: string;       // اسم المستخدم الذي قام بالتعليق
+    content: string;    // محتوى التعليق أو الإشعار
+  };
+  created_at: string;   // تاريخ الإشعار
+}
+
+interface PaginatedNotifications {
+  current_page: number;
+  data: Notification[]; 
+  last_page: number;
+  total: number;
+}
 
 @Component({
   standalone: true,
@@ -14,22 +30,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterModule],
 })
 export class NotificationComponent implements OnInit {
-  notifications: any[] = []; 
+  notifications: Notification[] = [];
+  errorMessage: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
 
-  constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService, private router: Router) {}
 
   ngOnInit(): void {
-    this.notificationService.getNotifications().subscribe({
-      next: (data: any) => {
-        this.notifications = data;
+    this.loadNotifications(this.currentPage);
+  }
+
+  loadNotifications(page: number): void {
+    this.notificationService.getNotifications(page).subscribe({
+      next: (response: any) => {
+        console.log(response); 
+        this.notifications = response.data; 
+        this.currentPage = response.current_page;
+        this.totalPages = response.last_page;
       },
       error: (error) => {
         console.error('Error fetching notifications:', error);
-      },
-      complete: () => {
-        console.log('Notifications fetching completed');
+        this.errorMessage = 'Failed to load notifications. Please try again later.';
       }
     });
   }
-  
+
+  goToAuctionDetails(auctionId: string): void {
+    this.router.navigate(['/auction', auctionId]); // التنقل باستخدام auctionId
+  }
 }
